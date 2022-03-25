c2a8497f35310d0a0eb840a7ff9086371b199277
Yang Yang <yangyang.astro@gmail.com>
2022-03-26 04:39:34 +0800

    Add files via upload

--- js/dianzan.js
+++ js/dianzan.js
@@ -38,3 +38,42 @@ function sendGood(){
 //        e.stopImmediatePropagation();
 //    }
 //}
+
+$(function () {
+    $('.zan').click(function () {
+        var tag=document.createElement('span')
+        tag.style.color='blue';
+        tag.style.position='absolute';
+        tag.innerHTML='+1'
+
+        var top=5;
+        var left=5;
+        var fontsize=12;
+        var opacity=1;
+
+         tag.style.fontSize=fontsize+'px';
+         tag.style.top=top+'px';
+         tag.style.left=left+'px';
+         tag.opacity=opacity;
+         $(this).append(tag);
+
+        var obj=setInterval(function () {
+            top-=15;
+            left+=5;
+            fontsize+=12;
+            opacity-=0.2;
+
+            tag.style.fontSize=fontsize+'px';
+            tag.style.top=top+'px';
+            tag.style.left=left+'px';
+            tag.opacity=opacity;
+
+            if(opacity<0){
+                clearInterval(obj);
+                tag.remove();
+            }
+            },100)
+    })
+})
+
+
